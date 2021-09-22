8b8166fced2488211de24ee9f77fc37e7257d893
JJ Roberts-White <computerfido@gmail.com>
2021-09-22 13:14:46 +1000

    Fix undefined symbol errors, simple example loading an image.

--- Examples/Test/src/Main.cpp
+++ Examples/Test/src/Main.cpp
@@ -1,11 +1,26 @@
 #include <Arclight/Core/Logger.h>
+#include <Arclight/Core/Node.h>
+#include <Arclight/Core/ResourceManager.h>
+#include <Arclight/Graphics/Image.h>
+#include <Arclight/Graphics/Sprite.h>
+#include <Arclight/Graphics/Texture.h>
 
-extern "C"{
-void GameInit(){
+Arclight::Sprite* test;
+Arclight::Texture* tex;
+
+extern "C" {
+void GameInit() {
     Arclight::Logger::Debug("Starting Game!");
+
+    std::shared_ptr<Arclight::Image> res =
+        Arclight::ResourceManager::Instance().GetResource<Arclight::Image>("art.png");
+    assert(res.get());
+
+    tex = new Arclight::Texture(*res);
+    test = new Arclight::Sprite(*tex);
 }
 
-void GameRun(){
+void GameRun() {
 
 }
 }
