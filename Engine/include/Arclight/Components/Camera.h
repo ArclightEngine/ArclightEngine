bb3a73fe0438f352967de77055f09ec81f123ae7
JJ <fidojj@hotmail.com>
2023-05-31 15:24:09 +1000

    Return camera entity for camera2d_get_current

--- Engine/include/Arclight/Components/Camera.h
+++ Engine/include/Arclight/Components/Camera.h
@@ -5,15 +5,14 @@
 
 namespace Arclight {
 
-struct Camera2D {
-    Transform2D transform;
-};
+struct Camera2D {};
 
 void camera2d_set_as_current(World& world, Entity entity);
-Camera2D* camera2d_get_current(World& world);
+Entity camera2d_get_current(World& world);
 // Get the transformation the camera applies to objects,
 // not the transform of the camera in world space
-Transform2D camera2d_get_transformation(Camera2D* camera);
+Transform2D camera2d_get_transformation(Entity camera);
+Vector2f camera2d_top_left(Entity camera);
 
 void camera_system(float elapsed, World& world);
 
--- Engine/src/Components/Camera.cpp
+++ Engine/src/Components/Camera.cpp
@@ -1,5 +1,9 @@
 #include <Arclight/Components/Camera.h>
 
+#include <Arclight/Core/Logger.h>
+
+#include <assert.h>
+
 #include <entt/core/hashed_string.hpp>
 
 namespace Arclight {
@@ -9,27 +13,37 @@ struct Camera2DContext {
 };
 
 void camera2d_set_as_current(World& world, Entity entity) {
+    if(!world.has_all_of<Transform2D, Camera2D>(entity)){
+        Logger::Error("camera2d_set_as_current: entity requires Transform2D, Camera2D");
+        return;
+    }
+    
     world.ctx_set<Camera2DContext>(entity);
 }
 
-Camera2D* camera2d_get_current(World& world) {
+Entity camera2d_get_current(World& world) {
     auto* ctx = world.try_ctx<Camera2DContext>();
     if(ctx) {
-        return &world.get_component<Camera2D>(ctx->entity);
+        return ctx->entity;
     }
 
-    return nullptr;
+    return NullEntity;
 }
 
-Transform2D camera2d_get_transformation(Camera2D* camera) {
-    Transform2D transform = camera->transform;
-    transform.set_position(camera->transform.get_position() * -1.f);
+Transform2D camera2d_get_transformation(Entity ent) {
+    Transform2D transform = World::current().get_component<Transform2D>(ent);
+    transform.set_position(transform.get_position() * -1.f);
 
     return transform;
 }
 
+Vector2f camera2d_top_left(Entity ent) {
+    //auto c = World::current().get_component<Camera2D>(ent);
+    //auto t = World::current().get_component<Transform2D>(ent);
+}
+
 void camera_system(float elapsed, World& world) {
-    Camera2D* current = camera2d_get_current(world);    
+    
 }
 
 } // namespace Arclight
